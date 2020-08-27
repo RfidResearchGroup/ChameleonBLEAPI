81a7f46b60a7c77ef475aac6d3b70f47afc3dd18
DXL <64101226@qq.com>
2020-08-27 11:24:48 +0800

    Delete ConnectFailedCtxCallback.java

--- ConnectFailedCtxCallback.java
+++
@@ -1,9 +0,0 @@
-package com.proxgrind.chameleon.callback;
-
-import android.app.Activity;
-
-import java.io.Serializable;
-
-public interface ConnectFailedCtxCallback extends Serializable {
-    void onFailed(Activity context);
-}
