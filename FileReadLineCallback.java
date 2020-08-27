4ce4b9242c1c6c64947e9e477d1cb40afc8e96b0
DXL <64101226@qq.com>
2020-08-27 11:25:04 +0800

    Delete FileReadLineCallback.java

--- FileReadLineCallback.java
+++
@@ -1,9 +0,0 @@
-package com.proxgrind.chameleon.callback;
-
-public interface FileReadLineCallback {
-    //读取完成的结果数组!
-    void onReadFinish(String[] line);
-
-    //读取失败的消息!
-    void onReadFail(String msg);
-}
